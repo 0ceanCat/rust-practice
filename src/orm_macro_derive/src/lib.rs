3fabbccaec305d04b83d60b0c6b3d82784289ab9
OceanCat <al15020@hotmail.com>
2024-03-10 12:20:37 +0000

    removed unsued vars

--- src/orm_macro_derive/src/lib.rs
+++ src/orm_macro_derive/src/lib.rs
@@ -9,7 +9,7 @@ use std::iter::zip;
 use std::ops::Deref;
 use std::sync::Once;
 use quote::quote;
-use syn::{self, Data, Fields, Type};
+use syn::{self, Data, DataStruct, Fields, Type};
 use syn::DeriveInput;
 
 static ONCE: Once = Once::new();
@@ -22,9 +22,7 @@ fn init_types_map() {
     map.insert("usize", "INTEGER".to_string());
     map.insert("u32", "INTEGER".to_string());
     map.insert("f64", "FLOAT".to_string());
-    map.insert("&str", "TEXT".to_string());
     map.insert("String", "TEXT".to_string());
-    map.insert("&String", "TEXT".to_string());
     map.insert("bool", "BOOLEAN".to_string());
 
     unsafe {
@@ -55,6 +53,61 @@ pub fn my_default(input: TokenStream) -> TokenStream {
         panic!("Entity derive macro must use in struct");
     };
 
+    check_id(&s);
+
+    let types_map = get_types_map();
+    let mut fields_map = get_fields_map(&s, types_map);
+
+    let fields: Vec<&str> = fields_map.iter().map(|(k, v)| k.as_str()).collect();
+    let param_index: Vec<String> = (1..=fields_map.len()).map(|i| format!("?{}", i)).collect();
+    let insert_sql = format!("INSERT INTO {} ({}) VALUES ({})", table, fields.join(", "), param_index.join(", "));
+
+
+    let update: Vec<String> = zip(fields.iter().filter(|x| x.deref().deref() != "id"), &param_index[..param_index.len() - 1])
+                                .map(|(k, i)| format!("{}={}", k, i)).collect();
+
+    let update_sql = format!("UPDATE {} SET {} WHERE id=?{}", table, update.join(", "), param_index.len());
+
+    let delete_sql = format!("DELETE FROM {} WHERE id=?1", table);
+
+    let fields_ident: Vec<Ident> = fields.iter().map(|f| Ident::new(f, Span::call_site())).collect();
+    let field_index: Vec<usize> = (0..fields.len()).collect();
+    let fields_without_id: Vec<Ident> = fields.iter().filter(|f| f.deref().deref() != "id").map(|f| Ident::new(f, Span::call_site())).collect();
+
+    let select_sql = format!("SELECT {} FROM {}", fields.join(", "), table);
+    let gen = quote! {
+        impl Entity for #id {
+            fn persist(&self) {
+                database().execute(#insert_sql, (#(&self.#fields_ident), *));
+            }
+
+            fn delete(&self) {
+                database().execute(#delete_sql, (&self.id, ));
+            }
+
+            fn update(&self) {
+                database().execute(#update_sql, (#(&self.#fields_without_id), *, &self.id));
+            }
+
+            fn find<P>(query: &str, params: P) -> Result<Vec<Self>, Error> where P: Params, Self: Sized{
+                let mut stmt = database().prepare(&format!("{} WHERE {}", #select_sql, query))?;
+                let mut result = Vec::new();
+                let mut rows = stmt.query(params)?;
+                while let Some(row) = rows.next()? {
+                    let p = Self {
+                        #(#fields_ident: row.get(#field_index)?,)*
+                    };
+                    result.push(p);
+                };
+
+                Result::Ok(result)
+            }
+        }
+    };
+    gen.into()
+}
+
+fn check_id(s: &DataStruct) {
     let has_id = s.fields.iter().any(|f| {
         if let Some(ref field) = f.ident {
             field.to_string() == "id" // type? who cares
@@ -66,9 +119,10 @@ pub fn my_default(input: TokenStream) -> TokenStream {
     if !has_id {
         panic!("Entity struct must have `id` field");
     }
+}
 
-    let types_map = get_types_map();
-    let mut fields_map = Vec::new();
+fn get_fields_map(s: &DataStruct, types_map: &HashMap<&str, String>) -> Vec<(String, String)> {
+    let mut fields_map = vec![];
     if let Fields::Named(fields) = &s.fields {
         for field in &fields.named {
             if let Some(field_name) = &field.ident {
@@ -86,55 +140,5 @@ pub fn my_default(input: TokenStream) -> TokenStream {
             }
         }
     }
-
-    let lines: Vec<String> = fields_map.iter().map(|(k, v)| format!("{} {}", k, v)).collect();
-    let lines = lines.join(",\n");
-
-    let keys: Vec<&str> = fields_map.iter().map(|(k, v)| k.as_str()).collect();
-    let param_index: Vec<String> = (1..=fields_map.len()).map(|i| format!("?{}", i)).collect();
-    let insert_sql = format!("INSERT INTO {} ({}) VALUES ({})", table, keys.join(", "), param_index.join(", "));
-
-
-    let update: Vec<String> = zip(keys.iter().filter(|x| x.deref().deref() != "id"), &param_index[..param_index.len() - 1])
-                                .map(|(k, i)| format!("{}={}", k, i)).collect();
-
-    let update_sql = format!("UPDATE {} SET {} WHERE id=?{}", table, update.join(", "), param_index.len());
-
-    let delete_sql = format!("DELETE FROM {} WHERE id=?1", table);
-
-    let fields: Vec<Ident> = fields_map.iter().map(|(k, _)| Ident::new(k, Span::call_site())).collect();
-    let field_index: Vec<usize> = (0..fields_map.len()).collect();
-    let fields_without_id: Vec<Ident> = fields_map.iter().filter(|(k, _)| k.deref() != "id").map(|(k, _)| Ident::new(k, Span::call_site())).collect();
-
-    let select_sql = format!("SELECT {} FROM {}", keys.join(", "), table);
-    let gen = quote! {
-        impl Entity for #id {
-            fn persist(&self) {
-                database().execute(#insert_sql, (#(&self.#fields), *));
-            }
-
-            fn delete(&self) {
-                database().execute(#delete_sql, (&self.id, ));
-            }
-
-            fn update(&self) {
-                database().execute(#update_sql, (#(&self.#fields_without_id), *, &self.id));
-            }
-
-            fn find<P>(query: &str, params: P) -> Result<Vec<Self>, Error> where P: Params, Self: Sized{
-                let mut stmt = database().prepare(&format!("{} WHERE {}", #select_sql, query))?;
-                let mut result = Vec::new();
-                let mut rows = stmt.query(params)?;
-                while let Some(row) = rows.next()? {
-                    let p = Self {
-                        #(#fields: row.get(#field_index)?,)*
-                    };
-                    result.push(p);
-                };
-
-                Result::Ok(result)
-            }
-        }
-    };
-    gen.into()
+    fields_map
 }
